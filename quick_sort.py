ab38c3e093bc40e5371388e8e7c5ed24dfee864f
Ben Keoshkerian <80909224+benkeosh13@users.noreply.github.com>
2023-06-19 14:40:01 -0500

    Add files via upload

---
+++ quick_sort.py
@@ -0,0 +1,14 @@
+#quick sort algorithm 
+
+def quicksort(arr):
+    if len(arr) <= 1:
+        return arr
+    stack = []
+    stack.append((0, len(arr)-1))
+    while len(stack) > 0:
+        pass
+def main():
+    arr = [50,30,80,20,10,70]
+    empty_array = []
+    quicksort(empty_array)
+main()
